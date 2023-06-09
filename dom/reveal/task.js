9bd0351c882f2908b25cd9467f767216729f2e71
Vl-pod <podsevalow@gmail.com>
2023-06-09 14:35:34 +0400

    1 task fixed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var revealElements = document.getElementsByClassName("reveal");
+	let revealElements = document.getElementsByClassName("reveal");
 
 	function revealInView() {
-		for (var i = 0; i < revealElements.length; i++) {
-			var revealElement = revealElements[i];
-			var revealPosition = revealElement.getBoundingClientRect().top;
-			var windowHeight = window.innerHeight;
+		for (let i = 0; i < revealElements.length; i++) {
+			let revealElement = revealElements[i];
+			let revealPosition = revealElement.getBoundingClientRect().top;
+			let windowHeight = window.innerHeight;
 
 			if (revealPosition < windowHeight) {
 				revealElement.classList.add("reveal_active");

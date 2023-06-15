efa722f37316b9d391a987835b16f32a8e71653d
Vl-pod <podsevalow@gmail.com>
2023-06-15 16:00:26 +0400

    tool-tip Fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,32 +1,44 @@
-tooltips.forEach((element) => {
-	element.addEventListener('click', (event) => {
-	  event.preventDefault();
-	  event.stopPropagation();
-  
-	  const isActive = element.classList.contains('tooltip_active');
-  
-	  tooltips.forEach((tooltip) => {
-		tooltip.classList.remove('tooltip_active');
-	  });
-  
-	  if (!isActive) {
-		const tooltipText = element.getAttribute('title');
-		tooltipElement.textContent = tooltipText;
-  
-		const elementRect = element.getBoundingClientRect();
-		const tooltipHeight = tooltipElement.offsetHeight;
-		const tooltipWidth = tooltipElement.offsetWidth;
-		const tooltipTop = elementRect.top - tooltipHeight - 10;
-		const tooltipLeft = elementRect.left + elementRect.width / 2 - tooltipWidth / 2;
-  
-		tooltipElement.style.top = tooltipTop + 'px';
-		tooltipElement.style.left = tooltipLeft + 'px';
-  
-		tooltips.forEach((tooltip) => {
-		  tooltip.classList.remove('tooltip_active');
-		});
-  
-		element.classList.add('tooltip_active');
-	  }
-	});
+const tooltips = document.querySelectorAll('.has-tooltip');
+
+// Создаем элемент для подсказки
+const tooltipElement = document.createElement('div');
+tooltipElement.classList.add('tooltip');
+document.body.appendChild(tooltipElement);
+
+// Обработчик события клика
+function handleTooltipClick(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const isActive = this.classList.contains('tooltip_active');
+
+  tooltips.forEach((tooltip) => {
+    tooltip.classList.remove('tooltip_active');
+    tooltip.style.color = '#000';
   });
+
+  if (!isActive) {
+    const tooltipText = this.getAttribute('title');
+    tooltipElement.textContent = tooltipText;
+
+    const elementRect = this.getBoundingClientRect();
+    const tooltipHeight = tooltipElement.offsetHeight;
+    const tooltipWidth = tooltipElement.offsetWidth;
+    const tooltipTop = elementRect.top - tooltipHeight - 10;
+    const tooltipLeft = elementRect.left + elementRect.width / 2 - tooltipWidth / 2;
+
+    tooltipElement.style.top = tooltipTop + 'px';
+    tooltipElement.style.left = tooltipLeft + 'px';
+
+    this.classList.add('tooltip_active');
+    this.style.color = 'red';
+  } else {
+    this.classList.remove('tooltip_active');
+    this.style.color = '#000';
+  }
+}
+
+// Добавляем обработчик клика для каждой ссылки с классом "has-tooltip"
+tooltips.forEach((tooltip) => {
+  tooltip.addEventListener('click', handleTooltipClick);
+});
